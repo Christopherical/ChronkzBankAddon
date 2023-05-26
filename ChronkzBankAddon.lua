1de2dc2a1a2cf4878f615cc895a2ad511a4cce65
Chris Dixon <chris_dixon1989@hotmail.com>
2023-05-26 15:53:30 +0100

    sorted item names but not count

--- ChronkzBankAddon.lua
+++ ChronkzBankAddon.lua
@@ -4,17 +4,49 @@ frame:RegisterEvent("PLAYER_LOGOUT")
 
 frame:SetScript("OnEvent", function(self, event, arg1)
     if event == "ADDON_LOADED" and arg1 == "ChronkzBankAddon" then
-        -- Our saved variables, if they exist, have been loaded at this point.
-        -- local a = GetGuildBankTabInfo(1)
-        local tableThing = {}
-        for i=1,97,1 do
-            table.insert(tableThing, GetGuildBankItemLink(1,i))
+        if GuildBankItemNames == nil then
+            GuildBankItemNames = {}
         end
-        ItemNameThing = tableThing
-
-        GuildBankInfo = "Global"
-        GuildBankInfoLocal = "Local"
+        if GuildBankItemAmounts == nil then
+            GuildBankItemAmounts = {}
+        end
+        if Aa == nil then
+            Aa = {}
+        end
+        if Bb == nil then
+            Bb = {}
+        end
+        if Cc == nil then
+            Cc = {}
+        end
+        if Dd == nil then
+            Dd = {}
+        end
+        if Ee == nil then
+            Ee = {}
+        end
+    elseif event == "PLAYER_LOGOUT" then
+        -- local temporaryNameTable = {}
+        -- local temporaryCountTable = {}
+        -- for tab = 1, 4, 1 do
+        --     local temp1 = {}
+        --     for i = 1, 98, 1 do
+        --         if GetGuildBankItemLink(tab, i) ~= nil then
+        --             table.insert(temp1, GetGuildBankItemLink(tab, i))
+        --         else
+        --             table.insert(temp1, "No Item")
+        --         end
+        --     end
+        --     table.insert(temporaryNameTable, temp1)
+        -- end
+        -- GuildBankItemNames = temporaryNameTable
+        local a, b, c, d, e = GetGuildBankItemInfo(1, 1)
+        Aa = a
+        Bb = b
+        Cc = c
+        Dd = d
+        Ee = e
+        GuildBankItemNames = "fish"
+        GuildBankItemAmounts = "cheese"
     end
 end)
-
-
