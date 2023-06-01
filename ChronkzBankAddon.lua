634a6a7b0e410e9b9babf44c44ee838415f39a47
Christopherical <chris_dixon1989@hotmail.com>
2023-06-01 10:14:43 +0100

    some changes

--- ChronkzBankAddon.lua
+++ ChronkzBankAddon.lua
@@ -1,8 +1,17 @@
 local EventFrame = CreateFrame("Frame", "EventFrame")
+
+-- https://wowpedia.fandom.com/wiki/Events
+-- use /etrace in game to listen to events.
 EventFrame:RegisterEvent("ADDON_LOADED")
 EventFrame:RegisterEvent("PLAYER_LOGOUT")
 EventFrame:RegisterEvent("GUILDBANKBAGSLOTS_CHANGED")
 
+-- Addon to collect what items we have in our guild bank.
+-- Collects the names of the items via GetGuildBankItemLink in the PLAYER_LOGOUT event.
+-- Collects the amount of the items via GetGuildBankItemInfo in the GUILDBANKBAGSLOTS_CHANGED event.
+
+Tab = 4
+BankSlots = 98
 
 EventFrame:SetScript("OnEvent", function(self, event, arg1)
     if event == "ADDON_LOADED" and arg1 == "ChronkzBankAddon" then
@@ -16,29 +25,34 @@ EventFrame:SetScript("OnEvent", function(self, event, arg1)
             CombinedItemAmountTable = {}
         end
     elseif event == "GUILDBANKBAGSLOTS_CHANGED" then
-        local temp2 = {}
+        local temp = {}
+        LastScanned = date("Last Scanned On: %d/%m/%y at %H:%M:%S.")
 
-        for tab = 1, 4, 1 do
-            for i = 1, 98, 1 do
-                local a, b = GetGuildBankItemInfo(tab, i)
-                if a ~= nil then
-                    temp2[i + ((tab - 1) * 98)] = b
+        -- https://wowpedia.fandom.com/wiki/API_GetGuildBankItemInfo
+        -- texture, itemCount, locked, isFiltered, quality = GetGuildBankItemInfo(tab,slot)
+        for tab = 1, Tab, 1 do
+            for i = 1, BankSlots, 1 do
+                local texture, itemCount = GetGuildBankItemInfo(tab, i)
+                if texture ~= nil then
+                    temp[i + ((tab - 1) * 98)] = itemCount
                 else
-                    temp2[i + ((tab - 1) * 98)] = "No Item"
+                    temp[i + ((tab - 1) * 98)] = "No Item"
                 end
             end
         end
 
-        GuildBankItemAmounts = temp2
+        GuildBankItemAmounts = temp
     elseif event == "PLAYER_LOGOUT" then
         local temp1 = {}
 
-        for tab = 1, 4, 1 do
-            for i = 1, 98, 1 do
+        -- https://wowpedia.fandom.com/wiki/API_GetGuildBankItemLink
+        -- itemLink = GetGuildBankItemLink(tab,slot)
+        for tab = 1, Tab, 1 do
+            for i = 1, BankSlots, 1 do
                 if GetGuildBankItemLink(tab, i) ~= nil then
-                    temp1[i + ((tab - 1) * 98)] = GetGuildBankItemLink(tab, i)
+                    temp1[i + ((tab - 1) * BankSlots)] = GetGuildBankItemLink(tab, i)
                 else
-                    temp1[i + ((tab - 1) * 98)] = "No Item"
+                    temp1[i + ((tab - 1) * BankSlots)] = "No Item"
                 end
             end
         end
@@ -50,7 +64,7 @@ EventFrame:SetScript("OnEvent", function(self, event, arg1)
             table.insert(CombinedItemAmountTable, { GuildBankItemNames[i], GuildBankItemAmounts[i] })
         end
 
-        GuildBankItemNames = 0
-        GuildBankItemAmounts = 0
+        GuildBankItemNames = LastScanned
+        GuildBankItemAmounts = math.floor(GetGuildBankMoney() / 10000) -- GetGuildBankMoney() returns copper.
     end
 end)
